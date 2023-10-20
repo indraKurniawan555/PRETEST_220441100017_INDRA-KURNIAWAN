34d16728864b1ac6862bbb3ff79af0477eea7431
indraKurniawan555 <113221828+indraKurniawan555@users.noreply.github.com>
2023-10-20 08:14:46 +0700

    Add files via upload

---
+++ PRETEST_220441100017_INDRA KURNIAWAN/src/pretest_220441100017_indra/kurniawan/PRETEST_220441100017_INDRAKURNIAWAN.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pretest_220441100017_indra.kurniawan;
+
+/**
+ *
+ * @author LENOVO SLIM 3 R3
+ */
+public class PRETEST_220441100017_INDRAKURNIAWAN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
